462357646e86b9d84d1f1c4a021cbad102605731
Ben Claussen <benclaussen@gmail.com>
2018-05-22 16:50:51 -0400

    Changes - Add Remove-NetboxVirtualMachine - Rename Set-NetboxVirtualInterface - Update tests

--- Functions/Virtualization/Virtualization.ps1
+++ Functions/Virtualization/Virtualization.ps1
@@ -502,7 +502,7 @@ function New-NetboxVirtualMachine {
     InvokeNetboxRequest -URI $URI -Method POST -Body $URIComponents.Parameters
 }
 
-function Add-NetboxVirtualInterface {
+function Add-NetboxVirtualMachineInterface {
     [CmdletBinding()]
     param
     (
@@ -653,3 +653,85 @@ function Set-NetboxVirtualMachineInterface {
 
 
 #endregion SET commands
+
+
+#region REMOVE commands
+
+function Remove-NetboxVirtualMachine {
+<#
+    .SYNOPSIS
+        Delete a virtual machine
+    
+    .DESCRIPTION
+        Deletes a virtual machine from Netbox by ID
+    
+    .PARAMETER Id
+        Database ID of the virtual machine
+    
+    .PARAMETER Force
+        Force deletion without any prompts
+    
+    .EXAMPLE
+        PS C:\> Remove-NetboxVirtualMachine -Id $value1
+    
+    .NOTES
+        Additional information about the function.
+#>
+    
+    [CmdletBinding(ConfirmImpact = 'High',
+                   SupportsShouldProcess = $true)]
+    param
+    (
+        [Parameter(Mandatory = $true,
+                   ValueFromPipelineByPropertyName = $true)]
+        [uint16[]]$Id,
+        
+        [switch]$Force
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        foreach ($VMId in $Id) {
+            $CurrentVM = Get-NetboxVirtualMachine -Id $VMId -ErrorAction Stop
+            
+            if ($Force -or $pscmdlet.ShouldProcess("$($CurrentVM.Name)/$($CurrentVM.Id)", "Remove")) {
+                $Segments = [System.Collections.ArrayList]::new(@('virtualization', 'virtual-machines', $CurrentVM.Id))
+                
+                $URI = BuildNewURI -Segments $Segments
+                
+                InvokeNetboxRequest -URI $URI -Method DELETE
+            }
+        }
+    }
+    
+    end {
+        
+    }
+}
+
+
+#endregion REMOVE commands
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
