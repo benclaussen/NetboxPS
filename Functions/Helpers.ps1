b50375722eaf44f72dab6735a9091bf909980df9
Ben Claussen <benclaussen@gmail.com>
2018-05-23 11:08:42 -0400

    Add DCIM choices and tests - Update ValidateChoice to handle strings and booleans

--- Functions/Helpers.ps1
+++ Functions/Helpers.ps1
@@ -213,11 +213,11 @@ function GetChoiceValidValues {
 
 function ValidateChoice {
     [CmdletBinding()]
-    [OutputType([uint16])]
+    [OutputType([uint16],[string], [bool])]
     param
     (
         [Parameter(Mandatory = $true)]
-        [ValidateSet('Circuits', 'Extras', 'IPAM', 'Virtualization', IgnoreCase = $true)]
+        [ValidateSet('Circuits', 'DCIM', 'Extras', 'IPAM', 'Virtualization', IgnoreCase = $true)]
         [string]$MajorObject,
         
         [Parameter(Mandatory = $true)]
@@ -231,22 +231,43 @@ function ValidateChoice {
     
     Write-Verbose "Validating $ChoiceName"
     Write-Verbose "Checking '$ProvidedValue' against [$($ValidValues -join ', ')]"
-    
-    if ($ValidValues -inotcontains $ProvidedValue) {
+
+    # Coercing everything to strings for matching... 
+    # some values are integers, some are strings, some are booleans
+    # Join the valid values with a pipe as a delimeter, because some values have spaces
+    if (([string]($ValidValues -join '|') -split '\|') -inotcontains [string]$ProvidedValue) {
         throw "Invalid value '$ProvidedValue' for '$ChoiceName'. Must be one of: $($ValidValues -join ', ')"
     }
     
-    # Convert the ProvidedValue to the integer value
-    try {
-        $intVal = [uint16]"$ProvidedValue"
-    } catch {
-        # It must not be a number, get the value from the label
-        $intVal = [uint16]$script:NetboxConfig.Choices.$MajorObject.$ChoiceName.Where({
-                $_.Label -eq $ProvidedValue
-            }).Value
+    switch -wildcard ("$MajorObject/$ChoiceName") {
+        "Circuits" {
+            # This has things that are not integers
+        }
+        
+        "DCIM/*connection_status" {
+            # This has true/false values instead of integers
+            try {
+                $val = [bool]::Parse($ProvidedValue)
+            } catch {
+                # It must not be a true/false value
+                $val = $script:NetboxConfig.Choices.$MajorObject.$ChoiceName.Where({ $_.Label -eq $ProvidedValue }).Value
+            }
+            
+            return $val
+        }
+        
+        default {
+            # Convert the ProvidedValue to the integer value
+            try {
+                $intVal = [uint16]"$ProvidedValue"
+            } catch {
+                # It must not be a number, get the value from the label
+                $intVal = [uint16]$script:NetboxConfig.Choices.$MajorObject.$ChoiceName.Where({ $_.Label -eq $ProvidedValue }).Value
+            }
+            
+            return $intVal
+        }
     }
-    
-    return $intVal
 }
 
 
