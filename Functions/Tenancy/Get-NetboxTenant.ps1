a98495e5f433b0d770c1f0af16107a0a75dd4db2
Ben Claussen <claussen@neonet.org>
2020-10-02 10:37:27 -0400

    Add help block for Get-NetboxTenant

--- Functions/Tenancy/Get-NetboxTenant.ps1
+++ Functions/Tenancy/Get-NetboxTenant.ps1
@@ -13,6 +13,50 @@
 
 
 function Get-NetboxTenant {
+<#
+    .SYNOPSIS
+        Get a tenent from Netbox
+    
+    .DESCRIPTION
+        A detailed description of the Get-NetboxTenant function.
+    
+    .PARAMETER Name
+        The specific name of the tenant. Must match exactly as is defined in Netbox
+    
+    .PARAMETER Slug
+        The specific slug of the tenant. Must match exactly as is defined in Netbox
+    
+    .PARAMETER Id
+        The database ID of the tenant
+    
+    .PARAMETER Query
+        A standard search query that will match one or more tenants.
+    
+    .PARAMETER Group
+        The specific group as defined in Netbox. 
+    
+    .PARAMETER GroupID
+        The database ID of the group in Netbox
+    
+    .PARAMETER CustomFields
+        Hashtable in the format @{"field_name" = "value"} to search
+    
+    .PARAMETER Limit
+        Limit the number of results to this number
+    
+    .PARAMETER Offset
+        Start the search at this index in results
+    
+    .PARAMETER Raw
+        Return the unparsed data from the HTTP request
+    
+    .EXAMPLE
+        		PS C:\> Get-NetboxTenant
+    
+    .NOTES
+        Additional information about the function.
+#>
+    
     [CmdletBinding()]
     param
     (
