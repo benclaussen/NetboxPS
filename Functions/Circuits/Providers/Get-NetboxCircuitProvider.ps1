c844322e48edd3daf9e53ed640be678fa6347240
Ben Claussen <claussen@neonet.org>
2020-11-13 12:17:49 -0500

    Add Circuit cmdlets - New-NetboxCircuit - Get-NetboxCircuitProvider - Get-NetboxCircuitTermination - Get-NetboxCircuitType

---
+++ Functions/Circuits/Providers/Get-NetboxCircuitProvider.ps1
@@ -0,0 +1,72 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.181
+	 Created on:   	2020-11-04 12:06
+	 Created by:   	Claussen
+	 Organization: 	NEOnet
+	 Filename:     	Get-NetboxCircuitProvider.ps1
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+
+function Get-NetboxCircuitProvider {
+    [CmdletBinding(DefaultParameterSetName = 'Query')]
+    param
+    (
+        [Parameter(ParameterSetName = 'ById',
+                   Mandatory = $true)]
+        [uint16[]]$Id,
+        
+        [Parameter(ParameterSetName = 'Query',
+                   Mandatory = $false)]
+        [string]$Name,
+        
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Query,
+        
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Slug,
+        
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$ASN,
+        
+        [Parameter(ParameterSetName = 'Query')]
+        [string]$Account,
+        
+        [Parameter(ParameterSetName = 'Query')]
+        [uint16]$Limit,
+        
+        [Parameter(ParameterSetName = 'Query')]
+        [uint16]$Offset,
+        
+        [switch]$Raw
+    )
+    
+    switch ($PSCmdlet.ParameterSetName) {
+        'ById' {
+            foreach ($i in $ID) {
+                $Segments = [System.Collections.ArrayList]::new(@('circuits', 'providers', $i))
+                
+                $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters -SkipParameterByName "Id"
+                
+                $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+                
+                InvokeNetboxRequest -URI $URI -Raw:$Raw
+            }
+        }
+        
+        default {
+            $Segments = [System.Collections.ArrayList]::new(@('circuits', 'providers'))
+            
+            $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters
+            
+            $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+            
+            InvokeNetboxRequest -URI $URI -Raw:$Raw
+        }
+    }
+}
