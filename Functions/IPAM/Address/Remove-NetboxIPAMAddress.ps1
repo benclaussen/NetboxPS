2595c58eaab2ba541ddbe2596451e41edf24abbf
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-03-30 16:34:25 +0200

    (WIP) Enhance IPAM Address (#4)
    * Address(IPAM): Fix trailing whitespace (with Visual Code Formater)
    
    * Address(IPAM): Fix PSSA about Command accepts pipeline input but not defined a process block
    
    * Address(IPAM): Add dns_name parameter to Add and Set
    
    * Address(IPAM): all integer are [int] (not uint16)

--- Functions/IPAM/Address/Remove-NetboxIPAMAddress.ps1
+++ Functions/IPAM/Address/Remove-NetboxIPAMAddress.ps1
@@ -1,4 +1,4 @@
-﻿<#	
+﻿<#
 	.NOTES
 	===========================================================================
 	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.172
@@ -12,46 +12,46 @@
 #>
 
 function Remove-NetboxIPAMAddress {
-<#
+    <#
     .SYNOPSIS
         Remove an IP address from Netbox
-    
+
     .DESCRIPTION
         Removes/deletes an IP address from Netbox by ID and optional other filters
-    
+
     .PARAMETER Id
         Database ID of the IP address object.
-    
+
     .PARAMETER Force
         Do not confirm.
-    
+
     .EXAMPLE
         PS C:\> Remove-NetboxIPAMAddress -Id $value1
-    
+
     .NOTES
         Additional information about the function.
 #>
-    
+
     [CmdletBinding(ConfirmImpact = 'High',
-                   SupportsShouldProcess = $true)]
+        SupportsShouldProcess = $true)]
     param
     (
         [Parameter(Mandatory = $true,
-                   ValueFromPipelineByPropertyName = $true)]
-        [uint16[]]$Id,
-        
+            ValueFromPipelineByPropertyName = $true)]
+        [int[]]$Id,
+
         [switch]$Force
     )
-    
+
     process {
         foreach ($IPId in $Id) {
             $CurrentIP = Get-NetboxIPAMAddress -Id $IPId -ErrorAction Stop
-            
+
             $Segments = [System.Collections.ArrayList]::new(@('ipam', 'ip-addresses', $IPId))
-            
+
             if ($Force -or $pscmdlet.ShouldProcess($CurrentIP.Address, "Delete")) {
                 $URI = BuildNewURI -Segments $Segments
-                
+
                 InvokeNetboxRequest -URI $URI -Method DELETE
             }
         }
