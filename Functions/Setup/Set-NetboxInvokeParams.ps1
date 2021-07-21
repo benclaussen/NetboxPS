254a2798ac872ede6152461afe70ad1aad3dfcb3
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-07-21 22:41:26 +0200

    Add Get/Set netboxInvokeParms for Get and Set Invoke Params (array)
    Like -SkipCertificate, Timeout...

---
+++ Functions/Setup/Set-NetboxInvokeParams.ps1
@@ -0,0 +1,15 @@
+function Set-NetboxInvokeParams {
+    [CmdletBinding(ConfirmImpact = 'Low',
+        SupportsShouldProcess = $true)]
+    [OutputType([string])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [array]$InvokeParams
+    )
+
+    if ($PSCmdlet.ShouldProcess('Netbox Invoke Params', 'Set')) {
+        $script:NetboxConfig.InvokeParams = $InvokeParams
+        $script:NetboxConfig.InvokeParams
+    }
+}
