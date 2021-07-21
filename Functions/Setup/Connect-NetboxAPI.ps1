b164ffe0b45b7a42e410b1b16723e9e3b3b4a7a3
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-07-21 22:06:00 +0200

    Connect(Setup): Fix indent (using Visual code Formatter)

--- Functions/Setup/Connect-NetboxAPI.ps1
+++ Functions/Setup/Connect-NetboxAPI.ps1
@@ -1,75 +1,76 @@
 ﻿function Connect-NetboxAPI {
-<#
+    <#
     .SYNOPSIS
         Connects to the Netbox API and ensures Credential work properly
-    
+
     .DESCRIPTION
         Connects to the Netbox API and ensures Credential work properly
-    
+
     .PARAMETER Hostname
         The hostname for the resource such as netbox.domain.com
-    
+
     .PARAMETER Credential
         Credential object containing the API key in the password. Username is not applicable
-    
+
     .PARAMETER Scheme
         Scheme for the URI such as HTTP or HTTPS. Defaults to HTTPS
-    
+
     .PARAMETER Port
         Port for the resource. Value between 1-65535
-    
+
     .PARAMETER URI
         The full URI for the resource such as "https://netbox.domain.com:8443"
-    
+
     .EXAMPLE
         PS C:\> Connect-NetboxAPI -Hostname "netbox.domain.com"
-        
+
         This will prompt for Credential, then proceed to attempt a connection to Netbox
-    
+
     .NOTES
         Additional information about the function.
 #>
-    
+
     [CmdletBinding(DefaultParameterSetName = 'Manual')]
     param
     (
         [Parameter(ParameterSetName = 'Manual',
                    Mandatory = $true)]
         [string]$Hostname,
-        
+
         [Parameter(Mandatory = $false)]
         [pscredential]$Credential,
-        
+
         [Parameter(ParameterSetName = 'Manual')]
         [ValidateSet('https', 'http', IgnoreCase = $true)]
         [string]$Scheme = 'https',
-        
+
         [Parameter(ParameterSetName = 'Manual')]
         [uint16]$Port = 443,
-        
+
         [Parameter(ParameterSetName = 'URI',
                    Mandatory = $true)]
         [string]$URI
     )
-    
+
     if (-not $Credential) {
         try {
             $Credential = Get-NetboxCredential -ErrorAction Stop
-        } catch {
+        }
+        catch {
             # Credentials are not set... Try to obtain from the user
             if (-not ($Credential = Get-Credential -UserName 'username-not-applicable' -Message "Enter token for Netbox")) {
                 throw "Token is necessary to connect to a Netbox API."
             }
         }
     }
-    
+
     $null = Set-NetboxCredential -Credential $Credential
-    
+
     switch ($PSCmdlet.ParameterSetName) {
         'Manual' {
             $uriBuilder = [System.UriBuilder]::new($Scheme, $Hostname, $Port)
         }
-        
+
         'URI' {
             $uriBuilder = [System.UriBuilder]::new($URI)
             if ([string]::IsNullOrWhiteSpace($uriBuilder.Host)) {
@@ -77,35 +78,37 @@
             }
         }
     }
-    
+
     $null = Set-NetboxHostName -Hostname $uriBuilder.Host
     $null = Set-NetboxHostScheme -Scheme $uriBuilder.Scheme
     $null = Set-NetboxHostPort -Port $uriBuilder.Port
-    
+
     try {
         Write-Verbose "Verifying API connectivity..."
         $null = VerifyAPIConnectivity
-    } catch {
+    }
+    catch {
         Write-Verbose "Failed to connect. Generating error"
         Write-Verbose $_.Exception.Message
         if (($_.Exception.Response) -and ($_.Exception.Response.StatusCode -eq 403)) {
             throw "Invalid token"
-        } else {
+        }
+        else {
             throw $_
         }
     }
-    
+
     Write-Verbose "Caching API definition"
     $script:NetboxConfig.APIDefinition = Get-NetboxAPIDefinition
-    
+
     if ([version]$script:NetboxConfig.APIDefinition.info.version -lt 2.8) {
         $Script:NetboxConfig.Connected = $false
         throw "Netbox version is incompatible with this PS module. Requires >=2.8.*, found version $($script:NetboxConfig.APIDefinition.info.version)"
     }
-    
+
     $script:NetboxConfig.Connected = $true
     Write-Verbose "Successfully connected!"
-    
+
     #Write-Verbose "Caching static choices"
     #$script:NetboxConfig.Choices.Circuits = Get-NetboxCircuitsChoices
     #$script:NetboxConfig.Choices.DCIM = Get-NetboxDCIMChoices # Not completed yet
@@ -114,6 +117,6 @@
     ##$script:NetboxConfig.Choices.Secrets = Get-NetboxSecretsChoices    # Not completed yet
     ##$script:NetboxConfig.Choices.Tenancy = Get-NetboxTenancyChoices
     #$script:NetboxConfig.Choices.Virtualization = Get-NetboxVirtualizationChoices
-    
+
     Write-Verbose "Connection process completed"
 }
