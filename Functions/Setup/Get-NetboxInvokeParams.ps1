989c5eaac227bfab4dba45226527676f887f47d9
Ben Claussen <claussen@neonet.org>
2021-07-22 10:27:58 -0400

    Correct typo

--- Functions/Setup/Get-NetboxInvokeParams.ps1
+++ Functions/Setup/Get-NetboxInvokeParams.ps1
@@ -4,7 +4,7 @@ function Get-NetboxInvokeParams {
 
     Write-Verbose "Getting Netbox InvokeParams"
     if ($null -eq $script:NetboxConfig.InvokeParams) {
-        throw "Netbox Invoke Parms is not set! You may set it with Set-NetboxInvokeParams -InvokeParams ..."
+        throw "Netbox Invoke Params is not set! You may set it with Set-NetboxInvokeParams -InvokeParams ..."
     }
 
     $script:NetboxConfig.InvokeParams
