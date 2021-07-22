c48bfe78ad1fff034d33e56683a4170a6fbe31a6
Ben Claussen <claussen@neonet.org>
2021-07-22 10:48:41 -0400

    Add Get/Set timeout functions

---
+++ Functions/Setup/Get-NetboxTimeout.ps1
@@ -0,0 +1,13 @@
+﻿
+function Get-NetboxTimeout {
+    [CmdletBinding()]
+    [OutputType([uint16])]
+    param ()
+    
+    Write-Verbose "Getting Netbox Timeout"
+    if ($null -eq $script:NetboxConfig.Timeout) {
+        throw "Netbox Timeout is not set! You may set it with Set-NetboxTimeout -TimeoutSeconds [uint16]"
+    }
+    
+    $script:NetboxConfig.Timeout
+}
