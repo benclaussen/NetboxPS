218114d8fee9c50f4a40211c15ec4ddd993e5e28
Ben Claussen <claussen@neonet.org>
2021-07-22 10:48:41 -0400

    Add Get/Set timeout functions

---
+++ Functions/Setup/Set-NetboxTimeout.ps1
@@ -0,0 +1,17 @@
+﻿
+function Set-NetboxTimeout {
+    [CmdletBinding(ConfirmImpact = 'Low',
+                   SupportsShouldProcess = $true)]
+    [OutputType([uint16])]
+    param
+    (
+        [Parameter(Mandatory = $false)]
+        [ValidateRange(1, 65535)]
+        [uint16]$TimeoutSeconds = 30
+    )
+    
+    if ($PSCmdlet.ShouldProcess('Netbox Timeout', 'Set')) {
+        $script:NetboxConfig.Timeout = $TimeoutSeconds
+        $script:NetboxConfig.Timeout
+    }
+}
