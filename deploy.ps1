a608d6ebd7367bef4f7921ec61afc238bfc57ff7
Alexis La Goutte <alexis.lagoutte@gmail.com>
2021-09-10 17:35:49 +0200

    Fix PSSA deploy(.ps1) warning (#26)
    * Deploy: fix indent (remove trailing whitespace)
    
    * Deploy(.ps1): Remove PSSA warning about don't use Write-host
    
    * Deploy(.ps1): Remove PSSA warning unused variable
    
    false positive (it is used for ParameterSet...)

--- deploy.ps1
+++ deploy.ps1
@@ -1,19 +1,19 @@
 ﻿<#
     .SYNOPSIS
         Concatenate files into single PSM1 and PSD1 files
-    
+
     .DESCRIPTION
         Concatenate all ps1 files in the Functions directory, plus the root PSM1,
         into a single PSM1 file in the NetboxPS directory.
 
         By default, this script will increment version by 0.0.1
-    
+
     .PARAMETER SkipVersion
-        Do not increment the version. 
-    
+        Do not increment the version.
+
     .PARAMETER VersionIncrease
         Increase the version by a user defined amount
-    
+
     .PARAMETER NewVersion
         Override the new version with this version
 
@@ -37,14 +37,16 @@
         ===========================================================================
 #>
 [CmdletBinding(DefaultParameterSetName = 'IncreaseVersion')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSReviewUnusedParameter", "")]
 param
 (
     [Parameter(ParameterSetName = 'SkipVersion')]
     [switch]$SkipVersion,
-    
+
     [Parameter(ParameterSetName = 'IncreaseVersion')]
     [version]$VersionIncrease = "0.0.1",
-    
+
     [Parameter(ParameterSetName = 'SetVersion')]
     [version]$NewVersion
 )
@@ -68,14 +70,14 @@ $Counter = 0
 Write-Host "Concatenating [$($PS1Files.Count)] PS1 files from $FunctionPath"
 foreach ($File in $PS1Files) {
     $Counter++
-    
+
     try {
         Write-Host (" Adding file {0:D2}/{1:D2}: $($File.Name)" -f $Counter, $PS1Files.Count)
-        
+
         "`r`n#region File $($File.Name)`r`n" | Out-File -FilePath $ConcatenatedFilePath -Encoding utf8 -Append -ErrorAction Stop
-        
+
         Get-Content $File.FullName -Encoding UTF8 -ErrorAction Stop | Out-File -FilePath $ConcatenatedFilePath -Encoding utf8 -Append -ErrorAction Stop
-        
+
         "`r`n#endregion" | Out-File -FilePath $ConcatenatedFilePath -Encoding utf8 -Append -ErrorAction Stop
     } catch {
         Write-Host "FAILED TO WRITE CONCATENATED FILE: $($_.Exception.Message): $($_.TargetObject)" -ForegroundColor Red
@@ -98,30 +100,30 @@ switch ($PSCmdlet.ParameterSetName) {
     "SkipVersion" {
         # Dont do anything with the PSD
         Write-Host " Skipping version update, maintaining version [$CurrentVersion]"
-        
+
         break
     }
-    
+
     "IncreaseVersion" {
         # Calculate the new version
         [version]$NewVersion = "{0}.{1}.{2}" -f ($CurrentVersion.Major + $VersionIncrease.Major), ($CurrentVersion.Minor + $VersionIncrease.Minor), ($CurrentVersion.Build + $VersionIncrease.Build)
-        
+
         Write-Host " Updating version in PSD1 from [$CurrentVersion] to [$NewVersion]"
-        
+
         # Replace the version number in the content
         #$PSDContent -replace $CurrentVersion, $NewVersion | Out-File $PSScriptRoot\$ModuleName.psd1 -Encoding UTF8
         Update-ModuleManifest -Path "$PSScriptRoot\$ModuleName.psd1" -ModuleVersion $NewVersion
-        
+
         break
     }
-    
+
     "SetVersion" {
         Write-Host " Updating version in PSD1 from [$CurrentVersion] to [$NewVersion]"
-        
+
         # Replace the version number in the content
         #$PSDContent -replace $CurrentVersion, $NewVersion | Out-File $PSScriptRoot\$ModuleName.psd1 -Encoding UTF8
         Update-ModuleManifest -Path "$PSScriptRoot\$ModuleName.psd1" -ModuleVersion $NewVersion
-        
+
         break
     }
 }
