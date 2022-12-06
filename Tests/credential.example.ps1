d7c008871df0845d571ae453c4ff15454479a0e2
Alexis La Goutte <alexis.lagoutte@gmail.com>
2022-12-06 19:49:35 +0100

    Tests (integration) DCIM (#34)
    * Tests: Add Initial support of Integration (using Pester v5)
    
    * Site: Add Tests for New/Get/Remove (DCIM) Site

---
+++ Tests/credential.example.ps1
@@ -0,0 +1,13 @@
+#
+# Copyright 2021, Alexis La Goutte <alexis dot lagoutte at gmail dot com>
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Copy this file to credential.ps1 (on Tests folder) and change connection settings..
+
+$script:hostname = "10.44.23.213"
+$script:token = "aaaaaaaaaaaaaaaaaa"
+
+#Uncomment if you want to personnalize some setting
+#script:pester_site1 = "pester_site1"
