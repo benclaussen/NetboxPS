7925db838fe33760cca29cbb6b65e7f5d587511f
Ben Claussen <benclaussen@gmail.com>
2023-02-24 12:13:24 -0500

    Increment version to 1.7.1

--- NetboxPS/NetboxPS.psm1
+++ NetboxPS/NetboxPS.psm1
@@ -517,19 +517,10 @@ function Connect-NetboxAPI {
         Write-Verbose "Found compatible version [$($script:NetboxConfig.NetboxVersion.'netbox-version')]!"
     }
     
-    $script:NetboxConfig.ContentTypes = Get-NetboxContentType -Limit 500
-    
     $script:NetboxConfig.Connected = $true
     Write-Verbose "Successfully connected!"
-
-    #Write-Verbose "Caching static choices"
-    #$script:NetboxConfig.Choices.Circuits = Get-NetboxCircuitsChoices
-    #$script:NetboxConfig.Choices.DCIM = Get-NetboxDCIMChoices # Not completed yet
-    #$script:NetboxConfig.Choices.Extras = Get-NetboxExtrasChoices
-    #$script:NetboxConfig.Choices.IPAM = Get-NetboxIPAMChoices
-    ##$script:NetboxConfig.Choices.Secrets = Get-NetboxSecretsChoices    # Not completed yet
-    ##$script:NetboxConfig.Choices.Tenancy = Get-NetboxTenancyChoices
-    #$script:NetboxConfig.Choices.Virtualization = Get-NetboxVirtualizationChoices
+    
+    $script:NetboxConfig.ContentTypes = Get-NetboxContentType -Limit 500
 
     Write-Verbose "Connection process completed"
 }
