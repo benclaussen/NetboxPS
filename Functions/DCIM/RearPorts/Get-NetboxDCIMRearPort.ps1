15a2e98662b31e67dd11b5c92c5ebc5beda6c648
sheffsix <jim@beck81.co.uk>
2023-03-13 09:21:14 +0000

    Start work on Front and Rear ports

---
+++ Functions/DCIM/RearPorts/Get-NetboxDCIMRearPort.ps1
@@ -0,0 +1,37 @@
+﻿
+function Get-NetboxDCIMRearPort
+{
+    [CmdletBinding()]
+    [OutputType([pscustomobject])]
+    param
+    (
+        [uint16]$Limit,
+
+        [uint16]$Offset,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [uint16]$Id,
+
+        [uint16]$Name,
+
+        [string]$Device,
+
+        [uint16]$Device_Id,
+
+        [uint16]$Type,
+
+        [switch]$Raw
+    )
+
+    process
+    {
+
+        $Segments = [System.Collections.ArrayList]::new(@('dcim', 'rear-ports'))
+
+        $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters
+
+        $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+
+        InvokeNetboxRequest -URI $URI -Raw:$Raw
+    }
+}
