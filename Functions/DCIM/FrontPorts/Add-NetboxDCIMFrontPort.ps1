36b20f0d28870df9735f0d8aa0ede72959ca1480
sheffsix <jim@beck81.co.uk>
2023-03-14 11:55:54 +0000

    Added Add-NetboxDCIMFrontPort function

---
+++ Functions/DCIM/FrontPorts/Add-NetboxDCIMFrontPort.ps1
@@ -0,0 +1,44 @@
+﻿
+function Add-NetboxDCIMFrontPort
+{
+    [CmdletBinding()]
+    [OutputType([pscustomobject])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [uint16]$Device,
+
+        [uint16]$Module,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Name,
+
+        [string]$Label,
+
+        [Parameter(Mandatory = $true)]
+        [string]$Type,
+
+        [ValidatePattern('^[0-9a-f]{6}$')]
+        [string]$Color,
+
+        [Parameter(Mandatory = $true)]
+        [uint16]$Rear_Port,
+
+        [uint16]$Rear_Port_Position,
+
+        [string]$Description,
+
+        [bool]$Mark_Connected,
+
+        [uint16[]]$Tags
+
+    )
+
+    $Segments = [System.Collections.ArrayList]::new(@('dcim', 'front-ports'))
+
+    $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters
+
+    $URI = BuildNewURI -Segments $URIComponents.Segments
+
+    InvokeNetboxRequest -URI $URI -Body $URIComponents.Parameters -Method POST
+}
