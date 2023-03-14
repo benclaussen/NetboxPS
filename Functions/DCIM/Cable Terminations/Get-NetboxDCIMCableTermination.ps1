115b28a48d7de7c5068b8a7ac579440917d12825
sheffsix <jim@beck81.co.uk>
2023-03-14 13:05:25 +0000

    Init. functions for cables and cable terminations

---
+++ Functions/DCIM/Cable Terminations/Get-NetboxDCIMCableTermination.ps1
@@ -0,0 +1,38 @@
+﻿
+function Get-NetboxDCIMCableTermination
+{
+    [CmdletBinding()]
+    #region Parameters
+    param
+    (
+        [uint16]$Limit,
+
+        [uint16]$Offset,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [uint16[]]$Id,
+
+        [uint16]$Cable,
+
+        [string]$Cable_End,
+
+        [string]$Termination_Type,
+
+        [uint16]$Termination_ID,
+
+        [switch]$Raw
+    )
+
+    #endregion Parameters
+
+    process
+    {
+        $Segments = [System.Collections.ArrayList]::new(@('dcim', 'cable-terminations'))
+
+        $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters -SkipParameterByName 'Raw'
+
+        $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+
+        InvokeNetboxRequest -URI $URI -Raw:$Raw
+    }
+}
---
+++ Functions/DCIM/Cables/Get-NetboxDCIMCable.ps1
@@ -0,0 +1,58 @@
+﻿
+function Get-NetboxDCIMCable
+{
+    [CmdletBinding()]
+    #region Parameters
+    param
+    (
+        [uint16]$Limit,
+
+        [uint16]$Offset,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [uint16[]]$Id,
+
+        [string]$Label,
+
+        [string]$Termination_A_Type,
+
+        [uint16]$Termination_A_ID,
+
+        [string]$Termination_B_Type,
+
+        [UInt16]$Termination_B_ID,
+
+        [string]$Type,
+
+        [string]$Status,
+
+        [string]$Color,
+
+        [UInt16]$Device_ID,
+
+        [string]$Device,
+
+        [uint16]$Rack_Id,
+
+        [string]$Rack,
+
+        [uint16]$Location_ID,
+
+        [string]$Location,
+
+        [switch]$Raw
+    )
+
+    #endregion Parameters
+
+    process
+    {
+        $Segments = [System.Collections.ArrayList]::new(@('dcim', 'cables'))
+
+        $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters -SkipParameterByName 'Raw'
+
+        $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+
+        InvokeNetboxRequest -URI $URI -Raw:$Raw
+    }
+}
