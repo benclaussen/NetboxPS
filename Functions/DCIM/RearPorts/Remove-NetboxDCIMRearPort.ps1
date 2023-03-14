b8392dc49ca5c8ef2265080bb183240bfb549875
sheffsix <jim@beck81.co.uk>
2023-03-14 11:38:11 +0000

    Added Remove-NetboxDCIMRearPort function

---
+++ Functions/DCIM/RearPorts/Remove-NetboxDCIMRearPort.ps1
@@ -0,0 +1,42 @@
+﻿
+function Remove-NetboxDCIMRearPort
+{
+
+    [CmdletBinding(ConfirmImpact = 'High',
+        SupportsShouldProcess = $true)]
+    param
+    (
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [uint16[]]$Id,
+
+        [switch]$Force
+    )
+
+    begin
+    {
+
+    }
+
+    process
+    {
+        foreach ($RearPortID in $Id)
+        {
+            $CurrentPort = Get-NetboxDCIMRearPort -Id $RearPortID -ErrorAction Stop
+
+            if ($Force -or $pscmdlet.ShouldProcess("Name: $($CurrentPort.Name) | ID: $($CurrentPort.Id)", "Remove"))
+            {
+                $Segments = [System.Collections.ArrayList]::new(@('dcim', 'rear-ports', $CurrentPort.Id))
+
+                $URI = BuildNewURI -Segments $Segments
+
+                InvokeNetboxRequest -URI $URI -Method DELETE
+            }
+        }
+    }
+
+    end
+    {
+
+    }
+}
