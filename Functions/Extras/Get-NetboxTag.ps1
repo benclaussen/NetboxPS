ec5f9e810f532cb48c82accde899c2fc2d7ed3b0
Ben Claussen <46791633+benclaussen1@users.noreply.github.com>
2023-03-17 16:04:52 -0400

    v1.8.0 (#45)
    * Fixing Interface Type parameter (#42)
    
    * Fixing #41
    
    * Update `deploy.ps1` to remove trailing whitespaces from psd1
    
    * Increase version to 1.7.3
    
    * Fix location of whitespace cleanup in `deploy.ps1`
    
    * Device port support (#44)
    
    * Fixing #41
    
    * Correct for OTBS
    
    * Start work on Front and Rear ports
    
    * Revert "Start work on Front and Rear ports"
    
    This reverts commit 257709fe2af5af5fbe4496b2d5b68481d80bf101.
    
    * Fixing #41
    
    * Add-NetboxDCIMRearPort function
    
    * Revert "Revert "Start work on Front and Rear ports""
    
    This reverts commit 4f6625af2dd95a5797da0efcff40774095ad2e95.
    
    * Fix incorrect parameter variable types
    
    * Parameter type adjustments
    
    * Create function to get tag information
    
    * Added Mark_Connected parameter
    
    * Add Set-NetboxDCIMRearPort function
    
    * Added Remove-NetboxDCIMRearPort function
    
    * Added validate pattern for Color parameter
    
    * Added Add-NetboxDCIMFrontPort function
    
    * Fixed param list missing Force parameter
    
    * Added Set-NetboxDCIMFrontPort function
    
    * Added Remove-NetboxDCIMFrontPort function
    
    * Init. functions for cables and cable terminations
    
    * Reformatting using OTBS
    Also working on defining tags by slug as an extra parameter, which appears
    only in Add-NetboxDCIMRearPort currently
    
    * Remove Tags_Slug parameter
    In hindsight implementing would create extra work that could be hard to
    maintain. Can be easily achieved outside of the function.
    
    * Update psproj
    
    * Fix braces
    
    * Update Postman for Netbox v3.4.5
    
    * Update vscode settings
    
    * Update version 1.8.0
    
    ---------
    
    Co-authored-by: SheffSix <jbeck1867@gmail.com>
    Co-authored-by: Ben Claussen <benclaussen@gmail.com>
    Co-authored-by: James Beck <jim@beck81.co.uk>

---
+++ Functions/Extras/Get-NetboxTag.ps1
@@ -0,0 +1,31 @@
+﻿
+function Get-NetboxTag {
+    [CmdletBinding()]
+    [OutputType([pscustomobject])]
+    param
+    (
+        [uint16]$Limit,
+
+        [uint16]$Offset,
+
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [uint16]$Id,
+
+        [string]$Name,
+
+        [string]$Slug,
+
+        [switch]$Raw
+    )
+
+    process {
+
+        $Segments = [System.Collections.ArrayList]::new(@('extras', 'tags'))
+
+        $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters
+
+        $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+
+        InvokeNetboxRequest -URI $URI -Raw:$Raw
+    }
+}
