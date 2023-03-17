0049ba4985409d7916cd5fa53fa1477aed3af315
Ben Claussen <benclaussen@gmail.com>
2023-03-17 15:37:09 -0400

    Fix braces

--- Functions/DCIM/RearPorts/Set-NetboxDCIMRearPort.ps1
+++ Functions/DCIM/RearPorts/Set-NetboxDCIMRearPort.ps1
@@ -1,65 +1,59 @@
 ﻿
-function Set-NetboxDCIMRearPort
-{
+function Set-NetboxDCIMRearPort {
     [CmdletBinding(ConfirmImpact = 'Medium',
-        SupportsShouldProcess = $true)]
+                   SupportsShouldProcess = $true)]
     [OutputType([pscustomobject])]
     param
     (
         [Parameter(Mandatory = $true,
-            ValueFromPipelineByPropertyName = $true)]
+                   ValueFromPipelineByPropertyName = $true)]
         [uint16[]]$Id,
-
+        
         [uint16]$Device,
-
+        
         [uint16]$Module,
-
+        
         [string]$Name,
-
+        
         [string]$Label,
-
+        
         [string]$Type,
-
+        
         [ValidatePattern('^[0-9a-f]{6}$')]
         [string]$Color,
-
+        
         [uint16]$Positions,
-
+        
         [string]$Description,
-
+        
         [bool]$Mark_Connected,
-
+        
         [uint16[]]$Tags,
-
+        
         [switch]$Force
     )
-
-    begin
-    {
-
+    
+    begin {
+        
     }
-
-    process
-    {
-        foreach ($RearPortID in $Id)
-        {
+    
+    process {
+        foreach ($RearPortID in $Id) {
             $CurrentPort = Get-NetboxDCIMRearPort -Id $RearPortID -ErrorAction Stop
-
+            
             $Segments = [System.Collections.ArrayList]::new(@('dcim', 'rear-ports', $CurrentPort.Id))
-
+            
             $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters -SkipParameterByName 'Id'
-
+            
             $URI = BuildNewURI -Segments $Segments
-
-            if ($Force -or $pscmdlet.ShouldProcess("Rear Port ID $($CurrentPort.Id)", "Set"))
-            {
+            
+            if ($Force -or $pscmdlet.ShouldProcess("Rear Port ID $($CurrentPort.Id)", "Set")) {
                 InvokeNetboxRequest -URI $URI -Body $URIComponents.Parameters -Method PATCH
             }
         }
     }
-
-    end
-    {
-
+    
+    end {
+        
     }
 }
