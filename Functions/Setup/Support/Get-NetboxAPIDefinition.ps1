cceb8fe76510a11baf6b659c197c63c53e5895c1
Ben Claussen <benclaussen@gmail.com>
2023-11-09 11:07:43 -0500

    Update Get-NetboxAPIDefinition for new path - Add $Format parameter for json or yaml

--- Functions/Setup/Support/Get-NetboxAPIDefinition.ps1
+++ Functions/Setup/Support/Get-NetboxAPIDefinition.ps1
@@ -1,15 +1,21 @@
 ﻿
 function Get-NetboxAPIDefinition {
     [CmdletBinding()]
-    param ()
-
-    #$URI = "https://netbox.neonet.org/api/docs/?format=openapi"
-
-    $Segments = [System.Collections.ArrayList]::new(@('docs'))
-
-    $URIComponents = BuildURIComponents -URISegments $Segments -ParametersDictionary @{'format' = 'openapi' }
-
+    param
+    (
+        [ValidateSet('json', 'yaml', IgnoreCase = $true)]
+        [string]$Format = 'json'
+    )
+    
+    #$URI = "https://netbox.neonet.org/api/schema/?format=json"
+    
+    $Segments = [System.Collections.ArrayList]::new(@('schema'))
+    
+    $URIComponents = BuildURIComponents -URISegments $Segments -ParametersDictionary @{
+        'format' = $Format.ToLower()
+    }
+    
     $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters -SkipConnectedCheck
-
+    
     InvokeNetboxRequest -URI $URI
 }
